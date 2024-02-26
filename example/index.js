7c99401f6337c0ef8492a791fa5b80da73d60064
Dan Jarvis <danwakeem@gmail.com>
2024-02-25 23:14:48 -0600

    chore: Added wait

--- example/index.js
+++ example/index.js
@@ -1,24 +1,20 @@
 const { IMDbClient } = require('./sdk/node');
 
 (async () => {
-  try {
-    // Get first argument
-    let environment = process.argv[2];
-    if (environment) {
-      environment = `https://${environment}-imdb.demos.danwakeem.com/imdb`;
-    }
-    const client = new IMDbClient({
-      environment,
-    });
-    const res = await client.imdb.createMovie({
-      title: `The Shawshank Redemption ${Date.now()}`,
-      rating: 1994,
-    });
-    console.log(res);
-
-    const fetchedMovie = await client.imdb.getMovie(res.id);
-    console.log(fetchedMovie);
-  } catch(error) {
-    console.error(error);
+  // Get first argument
+  let environment = process.argv[2];
+  if (environment) {
+    environment = `https://${environment.trim()}-imdb.demos.danwakeem.com/imdb`;
   }
+  const client = new IMDbClient({
+    environment,
+  });
+  const res = await client.imdb.createMovie({
+    title: `The Shawshank Redemption ${Date.now()}`,
+    rating: 1994,
+  });
+  console.log(res);
+
+  const fetchedMovie = await client.imdb.getMovie(res.id);
+  console.log(fetchedMovie);
 })();
