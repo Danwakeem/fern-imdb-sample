c379e566af3cb7a70ebe06980d014f285cb68115
Dan Jarvis <Danwakeem@users.noreply.github.com>
2024-02-25 23:23:28 -0600

    feat: Create PR workflow (#1)

--- example/index.ts
+++ example/index.ts
@@ -1,17 +1,20 @@
 import { IMDbClient } from './sdk/node';
 
 (async () => {
-  try {
-    const client = new IMDbClient();
-    const res = await client.imdb.createMovie({
-      title: `The Shawshank Redemption ${Date.now()}`,
-      rating: 1994,
-    });
-    console.log(res);
-
-    const fetchedMovie = await client.imdb.getMovie(res.id);
-    console.log(fetchedMovie);
-  } catch(error) {
-    console.error(error);
+  // Get first argument
+  let environment = process.argv[2];
+  if (environment) {
+    environment = `https://${environment.trim()}-imdb.demos.danwakeem.com/imdb`;
   }
+  const client = new IMDbClient({
+    environment,
+  });
+  const res = await client.imdb.createMovie({
+    title: `The Shawshank Redemption ${Date.now()}`,
+    rating: 1994,
+  });
+  console.log(res);
+
+  const fetchedMovie = await client.imdb.getMovie(res.id);
+  console.log(fetchedMovie);
 })();
