63a2bca380b97bd7890e8213c9c023d9d178f595
Dan Jarvis <danwakeem@gmail.com>
2024-02-25 22:10:10 -0600

    chore: First commit

---
+++ example/index.js
@@ -0,0 +1,17 @@
+const { IMDbClient } = require('./sdk/node');
+
+(async () => {
+  try {
+    const client = new IMDbClient();
+    const res = await client.imdb.createMovie({
+      title: `The Shawshank Redemption ${Date.now()}`,
+      rating: 1994,
+    });
+    console.log(res);
+
+    const fetchedMovie = await client.imdb.getMovie(res.id);
+    console.log(fetchedMovie);
+  } catch(error) {
+    console.error(error);
+  }
+})();
